a3b92702c8c06941868f85028ae67a371be442a6
domman95 <dominiklasek95@gmail.com>
2021-04-03 19:02:27 +0200

    fix: moved functions folder to the root and create simply serverless function just for test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import Layout from '../components/layout';
 import Nav from '../components/nav';
 
+const Main = styled.main`
+  padding-top: 7rem;
+
+  p {
+    font-size: 2rem;
+  }
+`;
+
 export default function HomePage() {
+  const [alert, setAlert] = useState('nothing');
+
+  const test = () => {
+    fetch('/.netlify/functions/boop')
+      .then((res) => res.json())
+      .then((data) => {
+        const { message } = data;
+        setAlert(message);
+      });
+  };
+
   return (
     <Layout>
       <Nav />
+      <Main>
+        <button onClick={test}>Run Serverless Function</button>
+        <p>{alert}</p>
+      </Main>
     </Layout>
   );
 }
