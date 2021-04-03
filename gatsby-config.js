d1a130242003886767487d057db4b105a3c1002c
domman95 <dominiklasek95@gmail.com>
2021-04-04 01:47:52 +0200

    add options for gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,19 @@ module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-netlify-identity-widget',
-    'gatsby-plugin-netlify',
+    {
+      resolve: 'gatsby-plugin-netlify',
+      options: {
+        headers: {
+          '/*': [
+            'Basic-Auth: someuser:somepassword anotheruser:anotherpassword',
+          ],
+          '/dashboard': [
+            // matching headers (by type) are replaced by Netlify with more specific routes
+            'Basic-Auth: differentuser:differentpassword',
+          ],
+        },
+      },
+    },
   ],
 };
